88fda98f644fbf32e83ccb5cae6bc782255685fa
morelandjo <morlanjo@gmail.com>
2021-02-02 15:41:07 -0500

    refactor and add save
    Refactored hard coded variables into arrays to prepare for saving.
    
    Added save functionality.

--- scripts/math.js
+++ scripts/math.js
@@ -1,4 +1,4 @@
-pierogi = 0;
+/*pierogi = 0;
 pierogi_second = 0;
 dough_level = 0;
 presser_level = 0
@@ -18,6 +18,68 @@ boiler_title="Pierogi Boiler";
 dough_visible=0;
 presser_visible=0;
 boiler_visible=0;
+*/
+
+
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
+
+//jobs arr = per second multiplier, image, title, cost multiplier, base cost
+let jobs_arr = [
+    ['dough',[1,"images/baker-icon.png","Dough Maker",1.5,20]],
+    ['presser',[5,"images/former-icon.png","Pierogi Presser",2,1000]],
+    ['boiler',[20,"images/boiler-icon.png","Pierogi Boiler",3,10000]]
+];
+
+let player_arr = [
+    ['pierogi',0],
+    ['pierogi_second',0],
+    ['dough_level',0],
+    ['dough_visibility',0],
+    ['presser_level',0],
+    ['presser_visibility',0],
+    ['boiler_level',0],
+    ['boiler_visibility',0]
+];
+
+console.log(player_arr);
+
+if(localStorage.getItem("save") !== null){
+    player_arr = localStorage.getObj("save");
+}
+
+console.log(player_arr);
+
+//console.log(jobs_arr[0][1][1]);
+
+function get_value_from_object(object,search){
+    for (let i = 0; i < object.length; i++) {
+        if (object[i][0] == search){
+            //console.log(object[i][1]);
+            return object[i][1];
+        }
+    }
+}
+
+function set_value_from_object(object,search,value,position){
+    for (let i = 0; i < object.length; i++) {
+        if (object[i][0] == search){
+            if (position > 0){
+                object[i][1][position] = value;
+            }else{
+                object[i][1] = value;
+            }
+        }
+    }
+}
+
+//get_value_from_object(jobs_arr,"presser");
+
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -28,10 +90,10 @@ function str_to_num_math(x) {
 }
 
 function display(){
-    $( "#counter" ).text(numberWithCommas(pierogi));
+    $( "#counter" ).text(numberWithCommas(get_value_from_object(player_arr,"pierogi")));
 }
 function second_display(){
-    $( "#second" ).text(numberWithCommas(pierogi_second));
+    $( "#second" ).text(numberWithCommas(get_value_from_object(player_arr,"pierogi_second")));
 }
 
 function level_update(job, level){
@@ -40,10 +102,10 @@ function level_update(job, level){
 
 
 $( "#pierogi" ).on( "click", function() {
-    //pierogi_grow.pause();
     pierogi_click.restart();
     $('.theExp').remove();
-    pierogi = pierogi+1;
+    var current = get_value_from_object(player_arr,"pierogi")+1;
+    set_value_from_object(player_arr,"pierogi",current,0);
     display();
     $('.rogie-splosion').prepend('<img class="theExp" src="images/explode.gif'+'?a='+Math.random()+'" />'); 
 });
@@ -51,7 +113,7 @@ $( "#pierogi" ).on( "click", function() {
 
 
 function calc_cost(level,job){
-    var dough = 1.5;
+   /* var dough = 1.5;
     var presser = 2;
     var boiler = 3;
     
@@ -68,7 +130,11 @@ function calc_cost(level,job){
     }else if (job == "boiler"){
         var multi = boiler;
         var base = boiler_base;
-    }
+    }*/
+    
+    var the_value = get_value_from_object(jobs_arr,job);
+    var multi = the_value[3];
+    var base = the_value[4];
     
     var cost = base+(base*level*multi);
     var cost = cost;
@@ -76,7 +142,7 @@ function calc_cost(level,job){
 }
 
 function add_level(job){
-    if(job == "dough"){
+    /*if(job == "dough"){
         dough_level = dough_level+1;
         level_update(job, dough_level);
         var new_cost = calc_cost(dough_level,job);
@@ -88,41 +154,54 @@ function add_level(job){
         boiler_level=boiler_level+1;
         level_update(job, boiler_level);
         var new_cost = calc_cost(boiler_level,job);
-    }
+    }*/
+    
+    var the_value = get_value_from_object(player_arr,job+"_level")+1;
+    set_value_from_object(player_arr,job+"_level",the_value,0);
+    level_update(job, the_value);
+    
+    $("#"+job+"-cost").text(numberWithCommas(calc_cost(the_value,job)));
+    /*
     $("#dough-cost").text(numberWithCommas(calc_cost(dough_level,"dough")));
     $("#presser-cost").text(numberWithCommas(calc_cost(presser_level,"presser")));
     $("#boiler-cost").text(numberWithCommas(calc_cost(boiler_level,"boiler")));
-    
+    */
 }
 
 $( ".job" ).on( "click", function() {
     //console.log('click');
     var job = $(this).attr("job");
     //console.log(job);
-    if(job == "dough"){
+    /*if(job == "dough"){
         var level = dough_level;
     }else if(job == "presser"){
         var level = presser_level;
     }else if(job == "boiler"){
         var level = boiler_level;
-    }
+    }*/
+    var level = get_value_from_object(player_arr,job+"_level");
+    
     //get cost
     var cost = calc_cost(level,job);
-    console.log(cost);
+    var pierogi = get_value_from_object(player_arr,"pierogi");
     if (pierogi >= cost){
         pierogi = pierogi-cost;
+        set_value_from_object(player_arr,"pierogi",pierogi,0);
         display(pierogi);
         add_level(job);
     }
     
 });
 
+//this can be refactored
 function color_functions(){
     if(str_to_num_math($("#counter").text()) >= str_to_num_math($("#dough-cost").text())){
         $("#dough-cost").css('color', 'green');
-        dough_visible=1;
-        $("#dough-maker .job-image").attr("src",dough_image);
-        $("#dough-maker .job-title").text(dough_title);
+        //dough_visible=1;
+        set_value_from_object(player_arr,"dough_visibility",1,0);
+        var dough = get_value_from_object(jobs_arr,"dough");
+        $("#dough-maker .job-image").attr("src",dough[1]);
+        $("#dough-maker .job-title").text(dough[2]);
         $("#dough-maker .hidden-job").hide();
     }else{
         $("#dough-cost").css('color', 'red');
@@ -130,9 +209,11 @@ function color_functions(){
     
     if(str_to_num_math($("#counter").text()) >= str_to_num_math($("#presser-cost").text())){
         $("#presser-cost").css('color', 'green');
-        presser_visible=1;
-        $("#pierogi-presser .job-image").attr("src",presser_image);
-        $("#pierogi-presser .job-title").text(presser_title);
+        //presser_visible=1;
+        set_value_from_object(player_arr,"presser_visibility",1,0);
+        var presser = get_value_from_object(jobs_arr,"presser");
+        $("#pierogi-presser .job-image").attr("src",presser[1]);
+        $("#pierogi-presser .job-title").text(presser[2]);
         $("#pierogi-presser .hidden-job").hide();
     }else{
         $("#presser-cost").css('color', 'red');
@@ -140,33 +221,47 @@ function color_functions(){
     
     if(str_to_num_math($("#counter").text()) >= str_to_num_math($("#boiler-cost").text())){
         $("#boiler-cost").css('color', 'green');
-        boiler_visible=1;
-        $("#pierogi-boiler .job-image").attr("src",boiler_image);
-        $("#pierogi-boiler .job-title").text(boiler_title);
+        //boiler_visible=1;
+        set_value_from_object(player_arr,"boiler_visibility",1,0);
+        var boiler = get_value_from_object(jobs_arr,"boiler");
+        $("#pierogi-boiler .job-image").attr("src",boiler[1]);
+        $("#pierogi-boiler .job-title").text(boiler[2]);
         $("#pierogi-boiler .hidden-job").hide();
     }else{
         $("#boiler-cost").css('color', 'red');
     }
 }
 
-$("#dough-cost").text(numberWithCommas(calc_cost(dough_level,"dough")));
-$("#presser-cost").text(numberWithCommas(calc_cost(presser_level,"presser")));
-$("#boiler-cost").text(numberWithCommas(calc_cost(boiler_level,"boiler")));
+//these init lines can probably be refactored
+$("#dough-cost").text(numberWithCommas(calc_cost(get_value_from_object(player_arr,"dough_level"),"dough")));
+$("#presser-cost").text(numberWithCommas(calc_cost(get_value_from_object(player_arr,"presser_level"),"presser")));
+$("#boiler-cost").text(numberWithCommas(calc_cost(get_value_from_object(player_arr,"boiler_level"),"boiler")));
 
-$("#dough-level").text(dough_level);
-$("#presser-level").text(presser_level);
-$("#boiler-level").text(boiler_level);
+$("#dough-level").text(get_value_from_object(player_arr,"dough_level"));
+$("#presser-level").text(get_value_from_object(player_arr,"presser_level"));
+$("#boiler-level").text(get_value_from_object(player_arr,"boiler_level"));
 
-setInterval(function(){ 
-    var add_rogies = dough_level*dough_multiplier;
-    add_rogies = add_rogies+(presser_level*presser_multiplier);
-    add_rogies = add_rogies+(boiler_level*boiler_multiplier);
+setInterval(function(){
+    
+    //this can be refactored
+    var dough =get_value_from_object(jobs_arr,"dough");
+    var presser = get_value_from_object(jobs_arr,"presser");
+    var boiler = get_value_from_object(jobs_arr,"boiler");
+    
+    var add_rogies = get_value_from_object(player_arr,"dough_level")*dough[0];
+    add_rogies = add_rogies+(get_value_from_object(player_arr,"presser_level")*presser[0]);
+    add_rogies = add_rogies+(get_value_from_object(player_arr,"boiler_level")*boiler[0]);
     //console.log(pierogi+" "+add_rogies);
-    pierogi=pierogi+add_rogies;
-    pierogi_second = add_rogies;
+    var pierogi=get_value_from_object(player_arr,"pierogi")+add_rogies;
+    set_value_from_object(player_arr,"pierogi",pierogi,0);
+    set_value_from_object(player_arr,"pierogi_second",add_rogies,0);
+    //pierogi_second = add_rogies;
     display();
     second_display();
     color_functions();
+    
+    //save
+    localStorage.setObj("save", player_arr);
 }, 1000);
 
 
